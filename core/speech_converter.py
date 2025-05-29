b1f94b33ff53b749363f594a25b14452a9513f59
Amal Nimmy Lal <amallal020799@gmail.com>
2025-05-29 18:41:10 +0200

    feat : Added text to speech

--- core/speech_converter.py
+++ core/speech_converter.py
@@ -3,6 +3,7 @@ import sounddevice as sd
 import wavio as wv
 import os
 import time
+import pyttsx3
 
 def audio_to_text():
     """
@@ -48,5 +49,19 @@ def audio_to_text():
         if os.path.exists(recording_file):
             os.remove(recording_file)
 
+def text_to_audio(text):
+    engine=pyttsx3.init()
+    voices=engine.getProperty("voices")
+    engine.setProperty("rate",125)
+    engine.setProperty("voice",voices[1].id)
+    engine.say(text)
+    engine.runAndWait()
+
+
 if __name__ == "__main__":
     audio_to_text()
+    text_to_audio("""The key to effective software development lies in balancing technical excellence with practical solutions.
+                     In my experience at TechSolutions, 
+                     I implemented this philosophy by optimizing database queries which reduced load times by 40%. 
+                     I'm passionate about clean code and proper documentation, which has helped my teams maintain 
+                    systems efficiently over time. I'm excited to bring these skills to your cloud-based applications.""")
